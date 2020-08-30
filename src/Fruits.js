430ddb40dd26a9a3997df1936c02807810b2ef89
Sanda Adeola <sanda_adeola@yahoo.com>
2020-08-29 18:58:01 -0700

    added code files

---
+++ src/Fruits.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Fruits(){
+return(
+    <div>
+        <h3>These are my favourite Animations</h3>
+        <ul>
+            <li>Kung Fu Panda</li>
+            <li>Avatar</li>
+            <li>Rick and Morty</li>
+        </ul>
+    </div>
+)
+
+}
+
+export default Fruits 
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./index.css";
+function Home(){
+    return(
+        <div>
+            <div className="home menu2">
+                <button id="firstbtn">Introducing OLA PAY</button>
+            <h1>Global Payments Made<br></br> Easy for Africa</h1>
+            <p id="para">The easiest way to accept payments
+            and make instant<br></br> payouts for small businesses in Africa</p>
+            <button id="secondbtn">Create an account</button>
+        </div>
+        </div>
+    )
+    
+    }
+    
+    export default Home 
